91995534977d7eb6b3ab056689a02a239e0e6274
Charlie Highum <chighum41@gmail.com>
2021-11-29 00:30:29 -0700

    working through the API calls for current weather and five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,25 @@
+// link to secret API key
 var APIKey = config.APIKey;
-
 var storedCities = JSON.parse(localStorage.getItem("cities")) || [];
 var cityForm = $("#city-form");
 var cityList = $("#city-list");
+var today = moment();
 
+// when the user submits a city it shows the current weather and five day forecast
+// and also saves the city local storage
 function saveCity(event) {
   event.preventDefault();
   var cityInput = $("#city-input").val();
   storedCities.push(cityInput);
   localStorage.setItem("cities", JSON.stringify(storedCities));
-  getForecast(cityInput);
+  getWeather(cityInput);
   $("#city-input").val("");
   renderCities();
 }
 
-function getForecast(cityName) {
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    cityName +
-    "&appid=" +
-    APIKey;
-  fetch(queryURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-}
-
 cityForm.on("submit", saveCity);
 
+// functionality to show each local storage entry to the screen
 function renderCities() {
   cityList.text("");
   for (var i = 0; i < storedCities.length; i++) {
@@ -45,6 +34,7 @@ function renderCities() {
   }
 }
 
+// delete individual items from the list of cities
 function deleteItem(event) {
   event.preventDefault();
   var btnClicked = event.target;
@@ -60,3 +50,71 @@ function deleteItem(event) {
 cityList.on("click", deleteItem);
 
 renderCities();
+
+// makes two API calls to openweather
+// 1st for the five day forecast because it can take a city input
+// 2nd for the current weather because it needs lat/long coordinates from the 1st call
+function getWeather(cityName) {
+  var fiveDayURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&units=imperial&appid=" +
+    APIKey;
+  fetch(fiveDayURL)
+    .then(function (fiveDayResponse) {
+      return fiveDayResponse.json();
+    })
+    .then(function (fiveDayForecast) {
+      var midDayForecasts = fiveDayForecast.list.filter(function (inputs) {
+        return inputs.dt_txt.includes("12:00");
+      });
+      for (x = 0; x < 5; x++) {
+        var date;
+      }
+      console.log(fiveDayForecast);
+      var cityLat = fiveDayForecast.city.coord.lat;
+      var cityLong = fiveDayForecast.city.coord.lon;
+      var todayURL =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        cityLat +
+        "&lon=" +
+        cityLong +
+        "&units=imperial&appid=" +
+        APIKey;
+      return fetch(todayURL);
+    })
+    .then(function (todayResponse) {
+      return todayResponse.json();
+    })
+    .then(function (todayWeather) {
+      console.log(todayWeather);
+      var city = $("<h3>");
+      var todayDate = $("<h3>");
+      var todayIcon = $("<img>");
+      var todayTemp = $("<p>");
+      var todayHumidity = $("<p>");
+      var todayWind = $("<p>");
+      var todayUV = $("<p>");
+
+      city.text(cityName);
+      todayDate.text(today.format("MMMM Do, YYYY"));
+      todayIcon.attr(
+        "src",
+        "https://openweathermap.org/img/wn/" +
+          todayWeather.current.weather[0].icon +
+          "@2x.png"
+      );
+      todayTemp.text("Temperature: " + todayWeather.current.temp + "°F");
+      todayHumidity.text("Humidity: " + todayWeather.current.humidity + "%");
+      todayWind.text("Wind: " + todayWeather.current.wind_speed + "knots");
+      todayUV.text("UV Index: " + todayWeather.current.uvi);
+
+      $("#weather").append(city);
+      $("#weather").append(todayDate);
+      $("#weather").append(todayIcon);
+      $("#weather").append(todayTemp);
+      $("#weather").append(todayHumidity);
+      $("#weather").append(todayWind);
+      $("#weather").append(todayUV);
+    });
+}
